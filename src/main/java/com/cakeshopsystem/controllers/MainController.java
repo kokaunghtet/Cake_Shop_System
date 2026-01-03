a75577adf2aed843a1a441782d8ea03cc06f66ec
Hp <hyuyuhlaingtun@gmail.com>
2026-01-03 20:03:55 +0630

    main update

--- src/main/java/com/cakeshopsystem/controllers/MainController.java
+++ src/main/java/com/cakeshopsystem/controllers/MainController.java
@@ -1,9 +1,59 @@
 package com.cakeshopsystem.controllers;
 
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 
 public class MainController {
+
+    @FXML
+    private Hyperlink adminProductLink;
+
+    @FXML
+    private Hyperlink bookingLink;
+
+    @FXML
+    private Hyperlink customerOrderLink;
+
+    @FXML
+    private Hyperlink dashboardLink;
+
+    @FXML
+    private BorderPane mainContent;
+
     @FXML
     private StackPane mainStackPane;
+
+    @FXML
+    private Hyperlink memberLink;
+
+    @FXML
+    private FontAwesomeIconView moonIcon;
+
+    @FXML
+    private ImageView profileImageView;
+
+    @FXML
+    private Label roleLabel;
+
+    @FXML
+    private FontAwesomeIconView settingIcon;
+
+    @FXML
+    private Hyperlink staffProductLink;
+
+    @FXML
+    private HBox topBar;
+
+    @FXML
+    private Label usenameLabel;
+
+    @FXML
+    private Hyperlink userLink;
+
 }
