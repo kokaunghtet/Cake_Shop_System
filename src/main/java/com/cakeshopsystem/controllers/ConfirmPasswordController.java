224baed3f6cf2c8c3123937e6c388f3b443e5295
Zune <zunez5697@gmail.com>
2026-01-04 22:19:28 -0800

    confirm and forgot pwd

---
+++ src/main/java/com/cakeshopsystem/controllers/ConfirmPasswordController.java
@@ -0,0 +1,32 @@
+package com.cakeshopsystem.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+
+public class ConfirmPasswordController {
+
+    @FXML
+    private Button btnConfirm;
+
+    @FXML
+    private StackPane confirmPasswordStackPane;
+
+    @FXML
+    private Hyperlink hyperLinkBackToLogin;
+
+    @FXML
+    private TextField txtConfirmPwd;
+
+    @FXML
+    private TextField txtPwd;
+
+    @FXML
+    void clickBtnConfirm(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/com/cakeshopsystem/controllers/ForgotPasswordController.java
@@ -0,0 +1,40 @@
+
+package com.cakeshopsystem.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ForgotPasswordController {
+
+    @FXML
+    private Button btnVerifyOTP;
+
+    @FXML
+    private Label lblTimer;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtOTP;
+
+    @FXML
+    void clickBacktoLogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickSendOTP(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickVerifyOTP(ActionEvent event) {
+
+    }
+
+}
+
