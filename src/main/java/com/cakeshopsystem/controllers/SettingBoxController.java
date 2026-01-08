20f4d7256d1db01345ddd6de5de2d8ab9d1fc18e
Karia229mwe <win2772006@gmail.com>
2026-01-08 15:10:50 +0630

    Add SettingBox.fxml & SettingBoxController

---
+++ src/main/java/com/cakeshopsystem/controllers/SettingBoxController.java
@@ -0,0 +1,14 @@
+package com.cakeshopsystem.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+
+public class SettingBoxController {
+
+    @FXML
+    private Hyperlink paymentConfigurationLink;
+
+    @FXML
+    private Hyperlink userConfigurationLink;
+
+}
