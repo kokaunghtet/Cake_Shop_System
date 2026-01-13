b324a59a6e6222d3671f8f4fa5773def7ebdcca4
Hp <hyuyuhlaingtun@gmail.com>
2026-01-13 14:50:44 +0630

    member view update

--- src/main/java/com/cakeshopsystem/controllers/admin/MemberViewController.java
+++ src/main/java/com/cakeshopsystem/controllers/admin/MemberViewController.java
@@ -1,18 +1,41 @@
 package com.cakeshopsystem.controllers.admin;
 
+import com.cakeshopsystem.models.Member;
+import com.cakeshopsystem.utils.cache.MemberCache;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 public class MemberViewController {
 
     @FXML
-    private TableColumn<?, ?> colName;
+    private TableColumn<Member, String> colName;
 
     @FXML
-    private TableColumn<?, ?> colPhoneNumber;
+    private TableColumn<Member, String> colPhoneNumber;
+
+    @FXML
+    private TableView<Member> memberTableView;
 
     @FXML
     private TextField txtSearch;
 
+    public void initialize() {
+        // Initialization code for MemberViewController
+
+        ObservableList<Member> memberList = MemberCache.getMembersList();
+        memberTableView.setItems(memberList);
+
+        configureTable();
+    }
+
+    private void configureTable() {
+        memberTableView.setFixedCellSize(50);
+
+        colName.setCellValueFactory(d -> new SimpleStringProperty(d.getValue().getMemberName()));
+        colPhoneNumber.setCellValueFactory(d -> new SimpleStringProperty(d.getValue().getPhone()));
+    }
 }
