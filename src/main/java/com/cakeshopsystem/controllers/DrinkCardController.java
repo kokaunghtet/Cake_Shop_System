8c628404a90ff5dd3990424ba65c1205d33ce498
Zune <zunez5697@gmail.com>
2026-01-12 22:50:21 -0800

    DrinkCard

---
+++ src/main/java/com/cakeshopsystem/controllers/DrinkCardController.java
@@ -0,0 +1,53 @@
+package com.cakeshopsystem.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class DrinkCardController {
+
+    @FXML
+    private Button btnAddtoCart;
+
+    @FXML
+    private Button btnColdDrink;
+
+    @FXML
+    private Button btnDecreaseDrink;
+
+    @FXML
+    private Button btnHotDrink;
+
+    @FXML
+    private Button btnIncreaseDrink;
+
+    @FXML
+    private ImageView drinkImage;
+
+    @FXML
+    void clickAddtoCartBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickColdBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickDecreaseBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickHotBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickIncreaseBtn(ActionEvent event) {
+
+    }
+
+}
