0b0f3dd70e14a1e797cedccbaaad37587b615724
Kaung Min Khant <kk8264483@gmail.com>
2026-01-14 15:51:36 +0630

    Add Product and Accesssories of fxml and controller

---
+++ src/main/java/com/cakeshopsystem/controllers/AccessoriesCardController.java
@@ -0,0 +1,22 @@
+package com.cakeshopsystem.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class AccessoriesCardController {
+
+    @FXML
+    private Button btnminus;
+
+    @FXML
+    private Button btnplus;
+
+    @FXML
+    private ImageView imgImage;
+
+    @FXML
+    private Label lbl;
+
+}
---
+++ src/main/java/com/cakeshopsystem/controllers/ProductCardController.java
@@ -0,0 +1,15 @@
+package com.cakeshopsystem.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class ProductCardController {
+
+    @FXML
+    private ImageView imgImage;
+
+    @FXML
+    private Label lbl;
+
+}
