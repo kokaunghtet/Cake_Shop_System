dc305ff7fdeb3dfdadaafcd2a4543e4e90517c7d
Hp <hyuyuhlaingtun@gmail.com>
2026-01-16 17:51:49 +0630

    custom order fxml

---
+++ src/main/java/com/cakeshopsystem/controllers/product/CustomOrderController.java
@@ -0,0 +1,11 @@
+package  com.cakeshopsystem.controllers.product;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+
+public class CustomOrderController {
+
+    @FXML
+    private ScrollPane customCakePane;
+
+}
